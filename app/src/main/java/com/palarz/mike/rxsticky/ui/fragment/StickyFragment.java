2ebecde866da56648f04540fdf96db8e2393c5c1
Mike Palarz <mpalarz1@gmail.com>
2019-06-01 13:41:02 -0500

    Added a TextView and Clear button
    I've added a TextView which displays the broadcasts that are
    being logged by the app. There are also now 2 buttons:
    
    Subscribe button - toggles the subscription on/off
    Clear button - clears previous broadcasts. This button is disabled
    when we are subscribed.
    
    I'm also using a StringBuilder to efficiently concatenate the
    Strings that are emitted.

--- app/src/main/java/com/palarz/mike/rxsticky/ui/fragment/StickyFragment.java
+++ app/src/main/java/com/palarz/mike/rxsticky/ui/fragment/StickyFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.palarz.mike.rxsticky.R;
 
@@ -26,13 +27,20 @@ public class StickyFragment extends Fragment {
 
     public static final String TAG = StickyFragment.class.getSimpleName();
 
-    private Button mStickyButton;
+    // Fragment views and UI-related variables
+    private Button mSubscribeButton;
+    private Button mClearButton;
+    private TextView mBroadcastsTextview;
+    private StringBuilder mStringBuilder;
+
+    // Intent filters needed for broadcasts
     private final IntentFilter mBatteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
     private final IntentFilter mAirplaneModeFilter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
     private final IntentFilter mTimeFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
     private final IntentFilter mScreenOnFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
     private final IntentFilter mScreenOffFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
 
+    // RxJava observables and Rx-related variables
     private Observable<String> mBatteryObservable;
     private Observable<String> mAirplaneModeObservable;
     private Observable<String> mTimeObservable;
@@ -40,7 +48,7 @@ public class StickyFragment extends Fragment {
     private Observable<String> mScreenOffObservable;
     private Observable<String> mAllBroadcasts;
     private CompositeDisposable mDisposable = new CompositeDisposable();
-    private boolean mSubscriptionToggle;
+    private boolean mSubscriptionToggle;    // Toggles the subscription on/off
 
     public static StickyFragment newInstance() {
         StickyFragment fragment = new StickyFragment();
@@ -66,34 +74,61 @@ public class StickyFragment extends Fragment {
                 .subscribeOn(AndroidSchedulers.mainThread());
 
         mSubscriptionToggle = false;
+        mStringBuilder = new StringBuilder();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sticky, container, false);
-        mStickyButton = rootView.findViewById(R.id.sticky_button);
-        mStickyButton.setOnClickListener((View view) -> {
+        mBroadcastsTextview = rootView.findViewById(R.id.sticky_textview);
+
+        mSubscribeButton = rootView.findViewById(R.id.subscribe_button);
+        mSubscribeButton.setOnClickListener((View view) -> {
 
             mSubscriptionToggle = !mSubscriptionToggle;
 
             if (mSubscriptionToggle) {
                 mAllBroadcasts
-                        .doOnDispose(() -> Log.i(TAG, "Unsubscribed from all broadcasts"))
+                        .doOnDispose(() -> {
+                            Log.i(TAG, "Unsubscribed from all broadcasts");
+                            mBroadcastsTextview.setText(mStringBuilder);
+                            // Clearing the StringBuilder
+                            mStringBuilder.setLength(0);
+                            // Re-enabling the clear broadcasts button
+                            mClearButton.setEnabled(true);
+                            mSubscribeButton.setText(R.string.fragment_sticky_subscribe_button_before_subscribe_text);
+                        })
                         .subscribe(
-                        string -> Log.i(TAG, string),
-                        (error) -> Log.e(TAG, "Error occurred: " + error),
-                        // onComplete() should never be called since these broadcasts will always
-                        // be sent. I.e., we have a never-ending stream of events.
-                        () -> Log.i(TAG, "onComplete()"),
-                        disposable -> {
-                            Log.i(TAG, "Subscribed to all broadcasts");
-                            mDisposable.add(disposable);
-                        });
+                                // onNext()
+                                string -> {
+                                    Log.i(TAG, string);
+                                    mStringBuilder.append(string + "\n");
+                                    },
+                                // onError()
+                                (error) -> Log.e(TAG, "Error occurred: " + error),
+                                // onComplete() should never be called since these broadcasts will always
+                                // be sent. I.e., we have a never-ending stream of events.
+                                () -> Log.i(TAG, "onComplete()"),
+                                // onSubscribe()
+                                disposable -> {
+                                    Log.i(TAG, "Subscribed to all broadcasts");
+                                    mBroadcastsTextview.setText(R.string.fragment_sticky_textview_logging_broadcasts);
+                                    // We're listening to broadcasts, so let's not enable this button
+                                    mClearButton.setEnabled(false);
+                                    mSubscribeButton.setText(R.string.fragment_sticky_subscribe_button_during_subscription_text);
+                                    mDisposable.add(disposable);
+                                });
             } else {
                 mDisposable.clear();
             }
 
+        });
+
+        mClearButton = rootView.findViewById(R.id.clear_broadcasts_button);
+        mClearButton.setOnClickListener(view -> {
+            mBroadcastsTextview.setText(null);
+            mBroadcastsTextview.setHint(R.string.fragment_sticky_textview_hint);
         });
 
         return rootView;
